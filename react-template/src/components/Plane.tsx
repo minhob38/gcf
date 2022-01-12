2005521866c681a37f0b049946c0c39a511eb220
kwon, minho <minhob38@gmail.com>
2022-01-12 22:09:32 +0900

    set emotion

--- react-template/src/components/Plane.tsx
+++ react-template/src/components/Plane.tsx
@@ -1,5 +1,25 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import kfx from "../assets/images/kfx.jpeg";
 
-export default function Plane() {
-  return <div>Plane 🛩</div>;
-}
+/*
+emotion
+https://emotion.sh/docs/introduction
+*/
+const CSS = css`
+  font: normal normal 16px / 30px Noto Sans CJK KR;
+`;
+const Img = styled.img``;
+
+const Plane: React.FC = () => {
+  return (
+    <>
+      <div css={CSS}>Plane 🛩</div>
+      <Img src={kfx} alt="kfx" width={150} />
+    </>
+  );
+};
+
+export default Plane;

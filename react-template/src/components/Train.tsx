3142a0b5bed4fefbd8505b1ee941397fa141f82e
kwon, minho <minhob38@gmail.com>
2022-01-12 23:59:57 +0900

    add helmet

--- react-template/src/components/Train.tsx
+++ react-template/src/components/Train.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 /*
 tailwindcss
 https://tailwindcss.com/docs/installation
 */
 export default function Flight() {
-  return <div className="text-base font-mono">Train 🚂</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Train 🚂</title>
+        <meta name="description" content="train page ..." />
+      </Helmet>
+      <div className="text-base font-mono">Train 🚂</div>
+    </>
+  );
 }

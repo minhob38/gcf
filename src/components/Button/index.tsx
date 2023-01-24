348f59a6f5f361df597165ebef04bef52076ae04
minho <minhob38@gmail.com>
2023-01-25 01:07:17 +0900

    add nav button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,42 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+import * as colors from "@constants/colors";
+import * as fonts from "@constants/fonts";
+
+interface IProps extends IStyleProps {
+  title: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+interface IStyleProps {
+  width: string;
+  height: string;
+}
+
+const Wrapper = styled.button`
+  all: unset;
+  display: block;
+  background-color: ${colors.WHITE1};
+  width: ${(props: IStyleProps) => props.width};
+  height: ${(props: IStyleProps) => props.height};
+  border: 2px solid ${colors.YELLOW1};
+  border-radius: 8px;
+  font: ${fonts.BUTTON_2};
+  color: ${colors.BLACK1};
+  text-align: center;
+  /* &:hover {
+    background-color: #ebc7c7;
+    border: none;
+  } */
+  cursor: pointer;
+`;
+
+const Button: React.FC<IProps> = ({ title, width, height, onClick }) => {
+  return (
+    <Wrapper width={width} height={height} onClick={onClick}>
+      {title}
+    </Wrapper>
+  );
+};
+
+export default Button;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,8 +1,13 @@
 /** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+
 import Header from "@components/Header";
 import Content from "@components/Content";
+import Button from "@components/Button";
 import Scroll from "@components/Scroll";
+
 import * as size from "@constants/size";
+import * as margins from "@constants/margins";
 
 // const Wrapper = styled.div({
 //   width: "100%",
@@ -22,11 +27,25 @@ import * as size from "@constants/size";
 //   cursor: pointer;
 // `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 15px 0 15px 0;
+  padding: 0 ${margins.SIDE_MAIN_MARGIN};
+`;
+
 const Landing = () => {
   return (
     <>
       <Header title="GCF CAR"></Header>
       <Content top={size.HEADER_HEIGHT} bottom="0">
+        <ButtonContainer>
+          <Button title="Buy/New" width="100px" height="40px"></Button>
+          <Button title="Buy/Used" width="100px" height="40px"></Button>
+          <Button title="Rent/New" width="100px" height="40px"></Button>
+        </ButtonContainer>
         <Scroll direction="y" height="calc(100% - 127px - 10px)"></Scroll>
       </Content>
     </>

b0cc22aa4c946836b8d38e25e616ff7a0d170f1a
minho <minhob38@gmail.com>
2023-03-01 19:58:12 +0900

    change landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,12 +2,18 @@
 import styled from "@emotion/styled";
 import Header from "@components/common/Header";
 import Content from "@components/common/Content";
-import Button from "@components/Landing/Button";
+import Button from "@components/CarService/Button";
 import Scroll from "@components/common/Scroll";
 import * as size from "@constants/size";
 import * as margins from "@constants/margins";
 import CarCard from "@components/Landing/CarCard";
 import { ErrorBoundary } from "react-error-boundary";
+import pickUpImage from "@assets/images/pickup-service.jpg";
+import teleImage from "@assets/images/tele-service.png";
+import moveImage from "@assets/images/move-service.png";
+import carImage from "@assets/images/car-service.png";
+
+import Image from "@components/common/Image";
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -25,12 +31,37 @@ const CarCardContainer = styled.div`
 // = ButtonContainer height + margin을 줄 height
 const SCROLL_BOTTOM_MARGIN = 70 + 20;
 
+const ServiceContainer = styled.div`
+  display: flex;
+  margin: 0 ${margins.SIDE_MAIN_MARGIN} 0 ${margins.SIDE_MAIN_MARGIN};
+`;
+
+const ServiceBox = styled.div`
+  width: ;
+`;
+
 const Landing = () => {
   return (
     <>
       <Header title="GCF CAR" mode="logo"></Header>
       <Content top={size.HEADER_HEIGHT} bottom="0">
-        <ButtonContainer>
+        <ServiceContainer>
+          <ServiceBox>
+            <Image src={pickUpImage} alt="back" width="100%" />
+          </ServiceBox>
+          <ServiceBox>
+            <Image src={teleImage} alt="back" width="100%" />
+          </ServiceBox>
+        </ServiceContainer>
+        <ServiceContainer>
+          <ServiceBox>
+            <Image src={moveImage} alt="back" width="100%" />
+          </ServiceBox>
+          <ServiceBox>
+            <Image src={carImage} alt="back" width="100%" />
+          </ServiceBox>
+        </ServiceContainer>
+        {/* <ButtonContainer>
           <Button title="Buy/New" width="100px" height="40px"></Button>
           <Button title="Buy/Used" width="100px" height="40px"></Button>
           <Button title="Rent/New" width="100px" height="40px"></Button>
@@ -50,7 +81,7 @@ const Landing = () => {
             <CarCard />
             <CarCard />
           </CarCardContainer>
-        </Scroll>
+        </Scroll> */}
       </Content>
     </>
   );

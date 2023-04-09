4b8bd186c1d95dc9d8dd990a84570f5ee035c5c5
minho <minhob38@gmail.com>
2023-04-09 10:09:51 +0900

    set react query

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./index.css";
 import App from "./App";
 import store from "./store/store";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),

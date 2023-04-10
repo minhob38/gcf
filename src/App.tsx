60234d190a0f017c71760cf766f2f4a3370adc57
minho <minhob38@gmail.com>
2023-04-11 00:20:43 +0900

    refacotr folder tree

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import styled from "@emotion/styled";
 import Landing from "./pages/Landing";
 import * as colors from "@constants/colors";
-import Login from "pages/Login";
+import SignIn from "pages/SignIn";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorPage from "pages/Error";
 import CarService from "pages/CarService";
@@ -39,8 +39,8 @@ function App() {
             <Route path="/" element={<Landing />}></Route>
             <Route path="/gcf" element={<Landing />}></Route>
             {/* auth */}
-            <Route path="/sign-in" element={<Login />}></Route>
-            <Route path="/gcf/sign-in" element={<Login />}></Route>
+            <Route path="/sign-in" element={<SignIn />}></Route>
+            <Route path="/gcf/sign-in" element={<SignIn />}></Route>
             <Route path="/sign-up" element={<SignUp />}></Route>
             <Route path="/gcf/sign-up" element={<SignUp />}></Route>
             {/* pickup service */}

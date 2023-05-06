698436dd8a4648316f29694b1e9fa11f98906d0c
minho <minhob38@gmail.com>
2023-05-06 21:20:06 +0900

    update api server domain

--- src/configs/axios-config.ts
+++ src/configs/axios-config.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 // export const API_SERVER_ADDRESS = "http://3.39.126.180";
-export const API_SERVER_ADDRESS = "https://dev.onepick.info";
+export const API_SERVER_ADDRESS =
+  process.env.NODE_ENV === "production"
+    ? "https://api.onepick.info"
+    : "https://dev-api.onepick.info";
 
 const instance = axios.create({
   baseURL: "http://localhost:8000/api",

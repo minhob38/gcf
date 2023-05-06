ed34f93eb4975a7f1f7ac074545b40395195fbd3
minho <minhob38@gmail.com>
2023-05-06 21:40:30 +0900

    set dev

--- src/configs/axios-config.ts
+++ src/configs/axios-config.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 // export const API_SERVER_ADDRESS = "http://3.39.126.180";
-export const API_SERVER_ADDRESS =
-  process.env.NODE_ENV === "production"
-    ? "https://api.onepick.info"
-    : "https://dev-api.onepick.info";
+// export const API_SERVER_ADDRESS =
+//   process.env.NODE_ENV === "production"
+//     ? "https://api.onepick.info"
+//     : "https://dev-api.onepick.info";
+export const API_SERVER_ADDRESS = "https://dev-api.onepick.info";
 
 const instance = axios.create({
   baseURL: "http://localhost:8000/api",
